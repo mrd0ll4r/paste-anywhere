208e4169908cb1281c1f151a1adc33bfd967195c
Leo Balduf <baldufle@hu-berlin.de>
2018-01-27 16:59:15 +0100

    overlay: implement incoming join connection handling

--- src/overlay.rs
+++ src/overlay.rs
@@ -17,6 +17,7 @@ pub struct Overlay {
     // TODO have state, clipboard in one mutex
     state: Arc<Mutex<CopyClock>>,
     clipboard: Arc<Mutex<String>>,
+    seen_join_message_ids: Arc<Mutex<HashMap<MessageID, ()>>>,
 }
 
 impl Overlay {
@@ -35,6 +36,7 @@ impl Overlay {
                 &PeerID::new(&addr, local.port()),
             ))),
             clipboard: Arc::new(Mutex::new(String::new())),
+            seen_join_message_ids: Arc::new(Mutex::new(HashMap::new())),
         })
     }
 
@@ -62,7 +64,7 @@ impl Overlay {
         loop {
             let msg = conn.read_message();
             if let Err(e) = msg {
-                println!("join: read failed, assuming connection closed: {:?}", e);
+                println!("->join: unable to read: {}, assuming connection closed", e);
                 conn.close();
                 return;
             }
@@ -72,7 +74,7 @@ impl Overlay {
             if let MessageType::JoinResponse { target } = msg.message_type {
                 if !target.eq(&self.own_id) {
                     println!(
-                        "join: got wrong target, dropping connection. Got: {:?}",
+                        "->join: got wrong target, dropping connection. Got: {:?}",
                         msg
                     );
                     return;
@@ -82,7 +84,7 @@ impl Overlay {
                 available.push(msg.src_id);
             } else {
                 println!(
-                    "join: received wrong message type, dropping connection. Got: {:?}",
+                    "->join: received wrong message type, dropping connection. Got: {:?}",
                     msg
                 );
                 return;
@@ -97,21 +99,20 @@ impl Overlay {
             let mut join_conn = JoinConnection::open(&self.own_id, id, 8);
             match join_conn {
                 Ok(mut conn) => {
-                    println!("join: opened a connection to {:?}", id);
+                    println!("->join: opened a connection to {:?}", id);
                     self.perform_join_single(conn);
                 }
                 Err(e) => {
-                    println!("join: unable to connect to {:?}: {}", id, e);
+                    println!("->join: unable to connect to {:?}: {}", id, e);
                     continue;
                 }
             }
         }
 
-
         let mut available = self.available_ids.lock().unwrap();
         available.as_mut_slice().sort();
         available.dedup();
-        println!("join: got these peers: {:?}", available);
+        println!("->join: got these peers: {:?}", available);
         if available.len() == 0 {
             return Err(From::from("I know no peers"));
         }
@@ -125,14 +126,14 @@ impl Overlay {
         let own_id = self.own_id.clone();
         let state = self.state.clone();
         let clipboard = self.clipboard.clone();
+        let seen_message_ids = self.seen_join_message_ids.clone();
         thread::spawn(move || {
             let mut sock = s.lock().unwrap();
             loop {
                 let mut incoming = accept(&mut sock).unwrap();
                 println!(
                     "Incoming connection: {:?}, first message: {:?}",
-                    incoming.conn,
-                    incoming.first_msg
+                    incoming.conn, incoming.first_msg
                 );
 
                 match incoming.conn {
@@ -148,7 +149,13 @@ impl Overlay {
                         );
                     }
                     Connection::Join(mut c) => {
-                        Overlay::handle_join_connection(c, peers.clone(), own_id.clone());
+                        Overlay::handle_join_connection(
+                            c,
+                            peers.clone(),
+                            own_id.clone(),
+                            incoming.first_msg.clone(),
+                            seen_message_ids.clone(),
+                        );
                     }
                 }
             }
@@ -193,15 +200,13 @@ impl Overlay {
     }
 
     fn handle_p2p_connection(mut c: P2PConnection, own_id: PeerID, state: Arc<Mutex<CopyClock>>) {
-        thread::spawn(move || {
-            loop {
-                let msg = c.read_message();
-                match msg {
-                    Ok(m) => println!("received: {:?}", m),
-                    Err(e) => {
-                        println!("unable to read: {}", e);
-                        return;
-                    }
+        thread::spawn(move || loop {
+            let msg = c.read_message();
+            match msg {
+                Ok(m) => println!("received: {:?}", m),
+                Err(e) => {
+                    println!("unable to read: {}", e);
+                    return;
                 }
             }
         });
@@ -211,22 +216,86 @@ impl Overlay {
         mut c: JoinConnection,
         peers: Arc<Mutex<HashMap<PeerID, ()>>>,
         own_id: PeerID,
+        msg: Message,
+        seen_message_ids: Arc<Mutex<HashMap<MessageID, ()>>>,
     ) {
         thread::spawn(move || {
-            let peers = peers.lock().unwrap();
-            println!("{:?}", peers);
-            loop {
-                let msg = c.read_message();
-                match msg {
-                    Ok(m) => println!("received: {:?}", m),
-                    Err(e) => {
-                        println!("unable to read: {}", e);
-                        return;
+            {
+                let mut seen_message_ids = seen_message_ids.lock().unwrap();
+                if seen_message_ids.contains_key(&msg.message_id) {
+                    println!("<-join: I already saw this message ID, closing connection");
+                    c.close();
+                    return;
+                }
+                seen_message_ids.insert(msg.message_id, ());
+            }
+
+            if msg.ttl <= 1 {
+                println!(
+                    "<-join: msg.ttl is {}, will just reply and close connection",
+                    msg.ttl
+                );
+                let resp = c.respond(&own_id, &msg);
+                match resp {
+                    Ok(_) => println!("<-join: reply successful"),
+                    Err(e) => println!("<-join: unable to reply: {}", e),
+                }
+                c.close();
+                return;
+            }
+
+            let mut p: Vec<Endpoint> = Vec::new();
+            {
+                let peers = peers.lock().unwrap();
+                for peer in peers.keys() {
+                    p.push(peer.clone());
+                }
+            }
+            println!(
+                "<-join: message has ttl={}, will forward to these peers: {:?}",
+                msg.ttl, p
+            );
+
+            for ep in p.iter() {
+                println!("<-join: forwarding to {:?}", ep);
+                let mut conn = JoinConnection::forward(&ep, &msg);
+                if let Err(e) = conn {
+                    println!("<-join: unable to forward: {}", e);
+                    continue;
+                }
+                let mut conn = conn.unwrap();
+
+                loop {
+                    let msg = conn.read_message();
+                    if let Err(e) = msg {
+                        println!("<-join: unable to read: {}", e);
+                        conn.close();
+                        break;
+                    }
+                    let msg = msg.unwrap();
+                    if let MessageType::JoinResponse { target } = msg.message_type {
+                        let resp = c.forward_response(&msg, &target);
+                        match resp {
+                            Ok(_) => println!(
+                                "<-join: forwarded peer {:?} to peer {:?}",
+                                msg.src_id, target
+                            ),
+                            Err(e) => {
+                                println!("<-join: unable to forward: {}, closing", e);
+                                c.close();
+                                return;
+                            }
+                        }
+                    } else {
+                        println!("<-join: did not receive JoinResponse, got: {:?}", msg);
+                        conn.close();
+                        break;
                     }
                 }
             }
 
-            JoinConnection::open(&own_id, &own_id, 8);
+            println!("<-join: done forwarding, closing");
+            c.close();
         });
     }
 }
